822f7df551cff82c199f3d57ba0be3da711eef41
Alexander Borca-Tasciuc <alexborcat@gmail.com>
2018-04-27 20:04:56 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req,res,next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log + '\n', (error) => {if (error) { console.log('There was an error');}});
+	next();
+});
+// app.use((req,res,next) => {
+// 	res.render('maintence.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear',() => {return new Date().getFullYear();});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+hbs.registerHelper('getMinutes', () => {
+	var timeD = new Date();
+	var time = '';
+	time += timeD.getHours();
+	time += ':' + timeD.getMinutes();
+	return time;
+})
+app.get('/',(req,res) => {
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		currentYear: new Date().getFullYear(),
+		welcomeMessage: 'Hello, welcome to handlebar website'
+	});
+	//res.send('<h1>Hello Express</h1>');
+});
+app.get('/about', (req,res) => {
+	res.render('about.hbs',
+			{
+				pageTitle: 'About Page',
+				currentYear: new Date().getFullYear()
+			}
+		)
+});
+
+app.get('/bad',(req,res) => {
+	res.send({
+		errorMessage:'handling request'
+	});
+})
+app.listen(3000, () => {
+	console.log('Server is up on port 3000')
+});
